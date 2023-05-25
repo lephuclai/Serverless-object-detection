510ae2929274d03bda2bc895dd92044907630ca3
MinhChau137 <chau.ntm137@gmail.com>
2023-05-25 18:05:50 +0200

    Bo sung /api/stream/active

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import threading
 import time
 from cv2 import VideoCapture
 import cv2
-from flask import Flask, request,copy_current_request_context
+from flask import Flask, request, copy_current_request_context
 # import sys, os
 # sys.path.append(os.path.join(os.getcwd(),'python/'))
-import darknet 
+import darknet
 
 
 app = Flask(__name__)
-#app.app_context().push()
+# app.app_context().push()
+
+
 def image_detection(image_or_path, network, class_names, class_colors, thresh):
     # Darknet doesn't accept numpy images.
     # Create one with image we reuse for each detect
@@ -23,10 +25,24 @@ def image_detection(image_or_path, network, class_names, class_colors, thresh):
                                interpolation=cv2.INTER_LINEAR)
 
     darknet.copy_image_from_bytes(darknet_image, image_resized.tobytes())
-    detections = darknet.detect_image(network, class_names, darknet_image, thresh=thresh)
+    detections = darknet.detect_image(
+        network, class_names, darknet_image, thresh=thresh)
     darknet.free_image(darknet_image)
     image = darknet.draw_boxes(detections, image_resized, class_colors)
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB), detections
+
+
+@app.route('/api/stream/active/<source>/<int:time>', methods=['GET'])
+def handle_streaming_active_thread_init(source, time):
+    rtmp_streaming_url = source
+    time_to_detect = time
+    try:
+        th = threading.Thread(target=detect_streaming, args=(
+            rtmp_streaming_url, time_to_detect,))
+        th.start()
+    except:
+        print("error")
+    return 'OK', 200
 
 
 @app.route('/api/stream/<source>/<int:time>', methods=['GET'])
@@ -35,12 +51,13 @@ def handle_streaming_thread_init(source, time):
     time_to_detect = time
     try:
         th = threading.Thread(target=detect_streaming, args=(
-            rtmp_streaming_url,time_to_detect,))
+            rtmp_streaming_url, time_to_detect,))
         th.start()
     except:
         print("error")
     th.join()
     return 'OK', 200
+
 
 def detect_streaming(rtmp_streaming_url: str, time_to_detect: int):
     path = f"rtmp://{rtmp_streaming_url}/live/stream"
@@ -53,30 +70,31 @@ def detect_streaming(rtmp_streaming_url: str, time_to_detect: int):
             cv2.imwrite("frame.jpg", frame)
             frame_number += 1
             print("** HANDLE FRAME NUMBER : {}\n***TIMESTAMP: {}".format(
-                frame_number,time.strftime("%Y%m%d-%H%M%S")))
+                frame_number, time.strftime("%Y%m%d-%H%M%S")))
             print("RESULTS:")
             image_name = 'frame.jpg'
             image, detections = image_detection(
-                    image_name, network, class_names, class_colors, 0.25
+                image_name, network, class_names, class_colors, 0.25
             )
             darknet.print_detections(detections, True)
             # r = dn.detect(net, meta, b'frame.jpg')
             # print(r)
 
-            print()        
+            print()
             if time.monotonic() - start_time > time_to_detect:
                 break
     cap.release()
     return
+
 
 @app.route('/api/picture', methods=['POST'])
 def handle_picture_api():
     @copy_current_request_context
     def handle_picture():
         f = request.files['upload']
-        f.save(f.filename) 
+        f.save(f.filename)
         image, detections = image_detection(
-                    f.filename, network, class_names, class_colors, 0.25)
+            f.filename, network, class_names, class_colors, 0.25)
         darknet.print_detections(detections, True)
     try:
         th = threading.Thread(target=handle_picture, args=())
@@ -101,5 +119,6 @@ def terminate_process():
 
 
 if __name__ == '__main__':
-    network, class_names, class_colors = darknet.load_network('cfg/yolov4.cfg','cfg/coco.data','yolov4.weights')
+    network, class_names, class_colors = darknet.load_network(
+        'cfg/yolov4.cfg', 'cfg/coco.data', 'yolov4.weights')
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
